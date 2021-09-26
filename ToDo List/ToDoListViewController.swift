e60c83fdb0a723a63aaf4b834cd91ba483dfe5a0
Richard Shum <richard18shum@gmail.com>
2021-09-26 15:17:21 -0400

    5.2 Setting up a TableView

---
+++ ToDo List/ToDoListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Richard Shum on 9/26/21.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoArray = ["Learn Swift", "Build Apps", "Change the World", "Take a Vacation"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+
+extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
+    }
+    
+    
+}
