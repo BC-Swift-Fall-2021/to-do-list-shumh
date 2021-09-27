9beb34d1a18d21c046047afe390cb08ecd697b9d
Richard Shum <richard18shum@gmail.com>
2021-09-27 01:24:04 -0400

    5.6 structs

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Richard Shum on 9/27/21.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
